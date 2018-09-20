0ecee9e913aa2009590ecfcac397588fe8c9bf6c
rrison <rrisoncai@gmail.com>
2018-09-21 00:49:34 +0800

    update mnist.py
    Signed-off-by: rrison <rrisoncai@gmail.com>

--- tensorflow_playground/mnist.py
+++ tensorflow_playground/mnist.py
@@ -4,10 +4,53 @@ import numpy as np
 from tensorflow.examples.tutorials.mnist import input_data
 
 # load mnist data
-mnist = input_data.read_data_sets("~/tf/datasets/mnist/", one_hot=True)
+mnist = input_data.read_data_sets("datasets/mnist/", one_hot=True)
 
-print("training datda size: ", mnist.train.num_examples)
-print("validation data size: ", mnist.validation.num_examples)
-print("Testing data size: ", mnist.test.num_examples)
-# print("Example training data: ", mnist.train.images[0])
-# print("Example training data label: ", mnist.train.labels[0])
+# constant
+INPUT_NODE = 784
+OUTPUT_NODE = 10
+LAYER1_NODE = 500
+BATCH_SIZE = 32
+LEARNING_RATE_BASE = 0.8
+LEARNING_RATE_DECAY = 0.99
+REGULARIZATION_RATE = 1e-4
+TRAINING_STEPS = 30000
+MOVING_AVERAGE_DECAY = 0.99
+
+'''
+def inference(input_tensor, avg_class, weights1, biases1, weights2, biases2):
+    if avg_class == None:
+        layer1 = tf.nn.relu(tf.matmul(input_tensor, weight1) + biases1)
+        return tf.matmul(layer1, weights2) + biases2
+    else:
+        layer1 = tf.nn.relu(tf.matmul(input_tensor, avg_class.average(weights1)) + avg_class.average(biases1))
+        return tf.matmul(layer1, avg_class.average(weights2)) + avg_class.average(biases2)
+
+def train(mnist):
+    X = tf.placeholder(tf.float32, [None, INPUT_NODE], name='X')
+    y = tf.placeholder(tf.float32, [None, OUTPUT_NODE], name='y')
+
+    weights1 = tf.Variable(tf.truncaated_normal([INPUT_NODE, LAYER1_NODE], stddev=0.1))
+    biases1 = tf.Variable(tf.constant(0.1, shape=[LAYER1_NODE]))
+    weights2 = tf.Variable(tf.truncaated_normal([LAYER1_NODE, OUTPUT_NODE], stddev=0.1))
+    biases2 = tf.Variable(tf.constant(0.1, shape=[OUTPUT_NODE]))
+
+    y = inference(X, None, weights1, biases1, weights2, biases2)
+    global_step = tf.Variable(0, trainable=False)
+    Variable_averages = tf.train.ExponentialMovingAverage(MOVING_AVERAGE_DECAY, global_step)
+    variable_averages_op = variable_averages.apply(tf.trainable_variables())
+    average_y = inference(X, variable_averages, weights1, biases1, weights2, biases2)
+    cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(y, tf.argmax(y_, 1))
+    cross_entropy_mean = tf.reduce_mean(cross_entropy)
+    regularizer = tf.contrib.layers.l2_regularizer(REGULARIZATION_RATE)
+    regularization = regularizer(weights1) + regularizer(weights2)
+    loss = cross_entropy_mean + regularization
+    learing_rate = tf.train.exponential_decay(LEARNING_RATE_BASE, global_step, mnist.train.num_examples / BATCH_SIZE, LEARNING_RATE_DECAY)
+    train_step = tf.train.GradientDescentOptimizer(learing_rate).minimize(loss, global_step=global_step)
+    with tf.control_dependencies([train_step, variable_averages_op]):
+        train_op = tf.no_op(name='train')
+
+x_batch, y_batch = mnist.train.next_batch(batch_size)
+print("X shape: ", x_batch.shape)
+print("y shape: ", y_batch.shape)
+'''
